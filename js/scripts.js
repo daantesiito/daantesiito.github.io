8611550c025b2ac7b2a6608a85eaaba8da9f0f51
Dante Puddu <dantepuddu@hotmail.com>
2024-04-17 04:46:14 -0300

    arreglando

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
-$(document).ready(function() {
-    $('.arrow.prev').click(function() {
-        $('.carousel').animate({marginLeft: '-=340px'}, 500);
-    });
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.querySelector('.arrow.prev');
+const nextBtn = document.querySelector('.arrow.next');
+const imgs = document.querySelectorAll('.carousel img');
+let counter = 0;
 
-    $('.arrow.next').click(function() {
-        $('.carousel').animate({marginLeft: '+=340px'}, 500);
-    });
+// Calcula el ancho de una imagen incluyendo el margen
+const imgWidth = imgs[0].getBoundingClientRect().width;
+
+nextBtn.addEventListener('click', () => {
+    if (counter < Math.ceil(imgs.length / 3) - 1) {
+        counter++;
+        carousel.style.transform = `translateX(-${counter * imgWidth * 3}px)`;
+    }
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter > 0) {
+        counter--;
+        carousel.style.transform = `translateX(-${counter * imgWidth * 3}px)`;
+    }
 });
