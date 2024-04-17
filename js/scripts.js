35c8576deb8cb9fc52a04b46c6f51f0a86c82925
Dante Puddu <dantepuddu@hotmail.com>
2024-04-17 17:08:04 -0300

    arreglando

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
 const carousel = document.querySelector('.carousel');
 const imgs = document.querySelectorAll('.carousel img');
-const imgWidth = imgs[0].getBoundingClientRect().width;
-const imgsPerSlide = 2; // Mostrar solo dos imágenes a la vez
+const imgWidth = carousel.getBoundingClientRect().width / 2; // Cambiado para obtener el ancho del contenedor del carrusel
+const imgsPerSlide = 2;
 let currentIndex = 0;
 
-// Función para actualizar la posición del carrusel
 function updateCarousel() {
     const offset = -currentIndex * imgWidth * imgsPerSlide;
     carousel.style.transition = 'transform 0.4s ease-in-out';
     carousel.style.transform = `translateX(${offset}px)`;
 }
 
-// Función para manejar el clic en la flecha izquierda
 function prevSlide() {
     if (currentIndex > 0) {
         currentIndex -= 1;
@@ -21,7 +19,6 @@ function prevSlide() {
     updateCarousel();
 }
 
-// Función para manejar el clic en la flecha derecha
 function nextSlide() {
     if (currentIndex < Math.ceil(imgs.length / imgsPerSlide) - 1) {
         currentIndex += 1;
@@ -31,9 +28,7 @@ function nextSlide() {
     updateCarousel();
 }
 
-// Manejadores de eventos para las flechas
 document.querySelector('.arrow.prev').addEventListener('click', prevSlide);
 document.querySelector('.arrow.next').addEventListener('click', nextSlide);
 
-// Actualizar el carrusel al cargar la página
 updateCarousel();
