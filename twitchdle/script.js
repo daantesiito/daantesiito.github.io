d35c16965070d1dd5375db9926e54c0495f3c7c8
daantesiito <adantepuddu@gmail.com>
2024-12-01 04:18:37 -0300

    cambiando url de redirect

--- twitchdle/script.js
+++ twitchdle/script.js
@@ -210,7 +210,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     loginWithTwitchButton.addEventListener("click", () => {
         const clientId = '0oy4xx9zsvkxsbgwm6n0rmb28xtivy';
-        const redirectUri = 'http://localhost:8000/';
+        const redirectUri = 'https://daantesiito.github.io/twitchdle/';
         const scope = 'user:read:email';
         const responseType = 'token';
     
